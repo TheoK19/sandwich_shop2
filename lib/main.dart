de026b8aff6c2e4e56a1d21829a67b35aba37f94
koripamotheo15 <koripamotheo15@gmail.com>
2025-10-28 17:16:05 +0000

    Define the Main App Widget: App.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  //runApp(const MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
